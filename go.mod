7f0e449940a0323ceb9cb1ea0d5a2b1bf2cf7fc2
rnugent3 <richard.nugent.3@gmail.com>
2021-03-09 17:40:23 -0800

    developed hard wired skeleton draft

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rnugent3/aggregateDDF
 
 go 1.15
+
+require github.com/HenryGeorgist/go-statistics v0.0.0-20210125164034-3542879c2b55
--- main.go
+++ main.go
@@ -1,52 +1,41 @@
-
 package main
 
 import (
 	"fmt"
 	"math/rand"
 
+	"github.com/HenryGeorgist/go-statistics/data"
 	"github.com/HenryGeorgist/go-statistics/statistics"
-	
-
 )
-
-/*
-func main() {
-	fmt.Println(quote.Go())
-}
-*/
-
-
 
 // 1. Parameterize a triangular distribution
 
-
 // 2. generate a random number u[0,1]
-func randomNumber() {
+func main() {
 	const seed = 54321
 	src := rand.NewSource(seed)
 	rnd := rand.New(src)
 
-	const N = 1
+	const N = 1000
 	var values [N]float64
-	
-	triDist := make([]statistics.ContinuousDistribution, N)
-	triDist[0] = statistics.TriangularDistribution{Min: 10; MostLikely: 20; Max: 30}
-	triDist[1] = statistics.TriangularDistribution{Min: 12; MostLikely: 22; Max: 32}
+
+	triDist := statistics.TriangularDistribution{Min: 10, MostLikely: 21, Max: 32}
 
 	for i := 0; i < N; i++ {
 		probability := rnd.Float64()
-		val := statistics.TriangularDistribution.invCDF(probability)
-		values[i] := val
+		val := triDist.InvCDF(probability)
+		values[i] = val
 	}
+
+	histogram := data.Init(2, 10, 32)
+	for idx := range values {
+		histogram.AddObservation(values[idx])
+	}
+
+	min := histogram.InvCDF(0)
+	max := histogram.InvCDF(1)
+	median := histogram.InvCDF(.5)
+
+	fmt.Println(fmt.Sprintf("The min is %f, the max is %f, and the most likely is %f", min, max, median))
+
 }
-
-
-
-
-
-func
-// pull the value from dist based on a random number put into InvCDF
-
-
-*/
